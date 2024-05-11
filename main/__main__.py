9dfff1fdce7dd22e98b7d68c157851273f928d22
Jaat bhae <154989744+Narendra4hacking@users.noreply.github.com>
2024-05-11 11:43:05 +0530

    Update __main__.py

--- main/__main__.py
+++ main/__main__.py
@@ -17,7 +17,7 @@ botStartTime = time.time()
 
 
 print("Successfully deployed!")
-print("Bot Deployed : @MASOOM_BAALAK🇮🇳")
+print("Bot Deployed : @jaat_bhae_2")
 
 if __name__ == "__main__":
     from . import bot
